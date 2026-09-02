94905cb2a65eafb0db554e0200e687c4ed4088ad
Alfredo Isla <islatortuga123@gmail.com>
2026-09-02 15:10:58 -0600

    Actualizar invitados y fotografía

--- invitados.js
+++ invitados.js
@@ -17,22 +17,728 @@ const invitados = {
 
     },
 
+    "papas-alfredo": {
 
-    "juan": {
-
-        nombre: "Juan y María",
+        nombre: "Familia Lara Soto",
 
         boletos: 2
 
     },
 
+    "ricardo-isla": {
 
-    "familia-gonzalez": {
+        nombre: "Familia Isla",
 
-        nombre: "Familia González",
+        boletos: 4
+
+    },
+
+    "soto-flores": {
+
+        nombre: "Familia Soto Flores",
+
+        boletos: 4
+
+    },
+
+    "flores-ortiz": {
+
+        nombre: "Familia Flores",
+
+        boletos: 2
+
+    },
+
+    "flores-vera": {
+
+        nombre: "Familia Flores Vera",
+
+        boletos: 6
+
+    },
+
+    "flores-vera2": {
+
+        nombre: "Familia Flores",
+
+        boletos: 4
+
+    },
+
+    "fam-vera": {
+
+        nombre: "Familia Flores Vera",
 
         boletos: 5
 
+    },
+
+    "lozada-chavez": {
+
+        nombre: "Familia Lozada Chavez",
+
+        boletos: 4
+
+    },
+
+    "soto-navarro": {
+
+        nombre: "Familia Soto Navarro",
+
+        boletos: 10
+
+    },
+
+    "fam-miranda": {
+
+        nombre: "Familia Miranda",
+
+        boletos: 4
+
+    },
+
+    "carlitos": {
+
+        nombre: "Estimado Carlitos",
+
+        boletos: 2
+
+    },
+
+    "gregorio-martinez": {
+
+        nombre: "Estimado Gregorio",
+
+        boletos: 2
+
+    },
+
+    "perez-hernandez": {
+
+        nombre: "Familia Pérez Hernández",
+
+        boletos: 3
+
+    },
+
+    "perez-benitez1": {
+
+        nombre: "Familia Pérez Benitez",
+
+        boletos: 3
+
+    },
+
+    "perez-benitez2": {
+
+        nombre: "Familia Pérez Benitez",
+
+        boletos: 4
+
+    },
+
+    "perez-benitez": {
+
+        nombre: "Familia Pérez Benitez",
+
+        boletos: 4
+
+    },
+
+    "murillo-colunga": {
+
+        nombre: "Familia Murillo Colunga",
+
+        boletos: 5
+
+    },
+
+    "murillo-godinez": {
+
+        nombre: "Familia Murillo Godínez",
+
+        boletos: 3
+
+    },
+
+    "lara": {
+
+        nombre: "Familia Lara Pérez",
+
+        boletos: 2
+
+    },
+
+    "lara1": {
+
+        nombre: "Familia Lara Pérez",
+
+        boletos: 2
+
+    },
+
+    "rangel": {
+
+        nombre: "Familia Rangel",
+
+        boletos: 2
+
+    },
+
+    "quetza-balam": {
+
+        nombre: "Queridos Quet y Balam",
+
+        boletos: 2
+
+    },
+
+    "Ulises": {
+
+        nombre: "Estimado Ulises",
+
+        boletos: 1
+
+    },
+
+    "Memo": {
+
+        nombre: "Estimado Guillermo",
+
+        boletos: 1
+
+    },
+
+    "Juan": {
+
+        nombre: "Estimado Juan",
+
+        boletos: 1
+
+    },
+
+    "Isra": {
+
+        nombre: "Estimado Israel",
+
+        boletos: 2
+
+    },
+
+    "Güero": {
+
+        nombre: "Estimado Gabriel",
+
+        boletos: 1
+
+    },
+
+    "amarillo": {
+
+        nombre: "Familia Pérez Álvarez",
+
+        boletos: 3
+
+    },
+
+    "ibsen": {
+
+        nombre: "Familia Velasco",
+
+        boletos: 3
+
+    },
+
+    "jesus-soto": {
+
+        nombre: "Familia Soto",
+
+        boletos: 3
+
+    },
+    "fam-islas": {
+
+        nombre: "Familia Islas",
+
+        boletos: 7
+
+    },
+
+    "cristina-islas": {
+
+        nombre: "Familia Islas",
+
+        boletos: 2
+
+    },
+
+    "erick-islas": {
+
+        nombre: "Familia Islas",
+
+        boletos: 2
+
+    },
+
+    "fam-adan": {
+
+        nombre: "Familia Benitez",
+
+        boletos: 4
+
+    },
+
+    "fam-fernando": {
+
+        nombre: "Familia Benitez",
+
+        boletos: 3
+
+    },
+    "fam-becerra": {
+
+        nombre: "Familia Becerra",
+
+        boletos: 3
+
+    },
+
+    "fam-becerra1": {
+
+        nombre: "Familia Becerra",
+
+        boletos: 3
+
+    },
+    "fam-dany": {
+
+        nombre: "Estimado Daniel",
+
+        boletos: 2
+
+    },
+    "fam-becerra2": {
+
+        nombre: "Familia Becerra",
+
+        boletos: 2
+
+    },
+
+    "fam-manuel-becerra": {
+
+        nombre: "Familia Becerra",
+
+        boletos: 5
+
+    },
+
+    "fam-estela-becerra": {
+
+        nombre: "Familia Becerra",
+
+        boletos: 5
+
+    },
+    "papas-ely": {
+
+        nombre: "Familia Hernández Vázquez",
+
+        boletos: 2
+
+    },
+
+    "karla-hernandezs": {
+
+        nombre: "Estimada Karla",
+
+        boletos: 3
+
+    },
+
+    "boya": {
+
+        nombre: "Estimada Tía Boya",
+
+        boletos: 1
+
+    },
+
+    "larios-hernandez": {
+
+        nombre: "Familia Larios Hernández",
+
+        boletos: 4
+
+    },
+
+    "garcia-hernandez": {
+
+        nombre: "Familia García Hernández",
+
+        boletos: 3
+
+    },
+
+    "quintos-hernandez": {
+
+        nombre: "Familia Quintos Hernández",
+
+        boletos: 4
+
+    },
+
+    "hernandez-navarrete": {
+
+        nombre: "Familia Hernández Navarrete",
+
+        boletos: 3
+
+    },
+
+    "hernandez-sagaon": {
+
+        nombre: "Familia Hernández Sagaon",
+
+        boletos: 2
+
+    },
+
+    "alis": {
+
+        nombre: "Estimada Tía Alis",
+
+        boletos: 1
+
+    },
+
+    "hernandez-castro": {
+
+        nombre: "Familia Hernández Castro",
+
+        boletos: 2
+
+    },
+
+    "sara-hernandez": {
+
+        nombre: "Estimada Sara Hernández",
+
+        boletos: 3
+
+    },
+
+    "hernandez-corona": {
+
+        nombre: "Familia Hernández Corona",
+
+        boletos: 3
+
+    },
+
+    "hernandez-perez": {
+
+        nombre: "Familia Hernández Pérez",
+
+        boletos: 6
+
+    },
+
+    "montoya-hernandez": {
+
+        nombre: "Familia Montoya Hernández",
+
+        boletos: 3
+
+    },
+
+    "camarillo-hernandez": {
+
+        nombre: "Familia Camarillo Hernández",
+
+        boletos: 3
+
+    },
+
+    "montoya-hernandez1": {
+
+        nombre: "Familia Montoya Hernández1",
+
+        boletos: 5
+
+    },
+
+    "piña-fragoso": {
+
+        nombre: "Familia Piña Fragoso",
+
+        boletos: 2
+
+    },
+
+    "calvillo-piña": {
+
+        nombre: "Familia Calvillo Piña",
+
+        boletos: 4
+
+    },
+
+    "leon-vazquez": {
+
+        nombre: "Familia León Vázquez",
+
+        boletos: 3
+
+    },
+
+    "martinez-leon": {
+
+        nombre: "Familia Martínez León",
+
+        boletos: 4
+
+    },
+
+    "leon-vazquez1": {
+
+        nombre: "Familia León Vázquez",
+
+        boletos: 3
+
+    },
+
+    "maria": {
+
+        nombre: "Estimada María",
+
+        boletos: 1
+
+    },
+
+    "vazquez-anguiano": {
+
+        nombre: "Familia Vázquez Anguiano",
+
+        boletos: 4
+
+    },
+
+    "vazquez-anguiano1": {
+
+        nombre: "Familia Vázquez Anguiano1",
+
+        boletos: 4
+
+    },
+
+    "barrientos-vazquez": {
+
+        nombre: "Familia Barrientos Vázquez",
+
+        boletos: 2
+
+    },
+
+    "barrientos": {
+
+        nombre: "Familia Barrientos",
+
+        boletos: 4
+
+    },
+
+    "barrientos1": {
+
+        nombre: "Familia Barrientos",
+
+        boletos: 2
+
+    },
+
+    "hernandez3": {
+
+        nombre: "Familia Hernández",
+
+        boletos: 5
+
+    },
+
+    "segura-nicasio": {
+
+        nombre: "Familia Segura Nicasio",
+
+        boletos: 2
+
+    },
+
+    "edna": {
+
+        nombre: "Estimada Edna",
+
+        boletos: 2
+
+    },
+
+    "jonhy": {
+
+        nombre: "Estimado Jonhy",
+
+        boletos: 2
+
+    },
+
+    "karen": {
+
+        nombre: "Estimada Karen",
+
+        boletos: 2
+
+    },
+
+    "amanda": {
+
+        nombre: "Estimada Amanda",
+
+        boletos: 2
+
+    },
+
+    "said": {
+
+        nombre: "Estimado Said",
+
+        boletos: 2
+
+    },
+
+    "karlita": {
+
+        nombre: "Estimada Karla",
+
+        boletos: 2
+
+    },
+
+    "laura": {
+
+        nombre: "Estimada Laura",
+
+        boletos: 2
+
+    },
+
+    "olivera": {
+
+        nombre: "Familia Olivera",
+
+        boletos: 7
+
+    },
+
+    "fam-leonardo-leo": {
+
+        nombre: "Estimados Papás de Leonardo",
+
+        boletos: 3
+
+    },
+
+    "fam-katy-leo": {
+
+        nombre: "Estimados Papás de Katy",
+
+        boletos: 5
+
+    },
+
+    "mary": {
+
+        nombre: "Estimada Mary",
+
+        boletos: 4
+
+    },
+
+    "antolina": {
+
+        nombre: "Estimada Antolina",
+
+        boletos: 4
+
+    },
+
+    "polo": {
+
+        nombre: "Estimada Polo",
+
+        boletos: 3
+
+    },
+
+    "alicia": {
+
+        nombre: "Estimada Alicia",
+
+        boletos: 4
+
+    },
+
+    "carlos": {
+
+        nombre: "Estimado Carlos",
+
+        boletos: 4
+
+    },
+
+    "cele": {
+
+        nombre: "Estimada Cele",
+
+        boletos: 5
+
+    },
+
+    "siria": {
+
+        nombre: "Estimada Siria",
+
+        boletos: 3
+
+    },
+
+    "telma": {
+
+        nombre: "Estimada Telma",
+
+        boletos: 2
+
+    },
+
+    "consuelito": {
+
+        nombre: "Estimada Señora Consuelito",
+
+        boletos: 3
+
+    },
+
+    "cristi": {
+
+        nombre: "Estimada Cristi",
+
+        boletos: 4
+
+    },
+
+    "cristina": {
+
+        nombre: "Estimada señora Cristina",
+
+        boletos: 2
+
     }
 
+    
 };
